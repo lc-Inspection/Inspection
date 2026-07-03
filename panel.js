4fa4f134caf04d21637cfa7704d677c71506c44b
Inspecton <musadogn@gmail.com>
2026-07-03 14:26:03 +0300

    panel.js

--- panel.js
+++ panel.js
@@ -628,7 +628,7 @@ let animationEffect = 'slide'; // slide, fade, zoom, flip
 // APP CONFIG (Tüm Ayarlar)
 // ────────────────────────────
 const APP_CONFIG_KEY = 'lc_inspection_config';
-const DEFAULT_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbzmERIcWk5nNz3S3jpomWznWorXN2--TmbXXPScOKlwSnK75CArUDJjCgHBx8ZiCsZj/exec';
+const DEFAULT_SHEETS_URL = 'https://script.google.com/macros/s/AKfycbwU5JnXz5PVttOE-VoKx-tJze_zFPabeC0trhiyDj6BZ6ucw7F2jHIW43I3nxQ7urvQ/exec';
 const DEFAULT_API_TOKEN  = 'lcw-secret-2024';
 let appConfig = {
   password: '',          // Panel admin şifresi — Sheets Config'ten yüklenir, kodda saklanmaz
